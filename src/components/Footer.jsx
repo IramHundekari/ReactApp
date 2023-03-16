178a6bd91e0ed65122667e18069cd1069336b13a
Iram Hundekari <100361020+IramHundekari@users.noreply.github.com>
2023-03-17 00:30:08 +0530

    commit myapp

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all rights reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in
+          /iram-hundekari-8b573a19b" target={"blank"}>
+            LinkedIn
+          </a>
+
+          <a href="https://www.github.com/IramHundekari" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
